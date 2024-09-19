f63b5e0a028500ac6d16d70f85100314a5363a69
Danilo Jakob <danilojakob@protonmail.com>
2024-09-19 23:42:15 +0200

    feat: filling up ram with chars in this case. interestingly even though you allocate memory it will only get 'used' when something is stored there...which makes sense i guess

--- main.c
+++ main.c
@@ -13,6 +13,8 @@ char *allocate_string(const char *str) {
   return pointer;
 }
 
+void *kill_the_ram() { return malloc(20073741824); }
+
 int main(void) {
   const char *text = "Hello World"; // string stored in special read-only memory
   char another_text[] =
@@ -23,7 +25,15 @@ int main(void) {
 
   printf("%s\n", heap_text);
 
+  void *killer = kill_the_ram();
+  strcpy(killer, "kill");
+
+  while (1) {
+    memset(killer, 'd', 20073741824 * sizeof(char));
+  }
   // deallocate memory
   free(heap_text);
+  // deallocate memory for no real reason here what so ever
+  free(killer);
   return 0;
 }
